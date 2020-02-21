5c4106ec1bce839b6906c3c1f11b649b03bb5ab3
lorenzofox3 <laurent34azerty@gmail.com>
2020-02-21 11:01:58 +0100

    add test in the CI

--- app/test/test-runner.js
+++ app/test/test-runner.js
@@ -1,1 +1,16 @@
-// todo automate with pupeeter
+const puppeteer = require('puppeteer');
+
+(async () => {
+    try {
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+        const page = await browser.newPage();
+        page.on('console', ev => console.log(ev.text()));
+        await page.goto(`http://localhost:3002/test/dist/index.html`);
+        await browser.close();
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+})();
